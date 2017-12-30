a0cf33a2d26a3bca3a8e2de2ab26ffe72c704972
wyf <wyf@gmail.com>
2017-12-30 11:00:07 +0800

    换了下main

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import App from './App.vue';
 
 // 创建一个根实例
 new Vue({
-    // 渲染根组件 这个方法从导入的 Vue这个包中获得
-    render:function(creElement){
-        return creElement(App)
-    }
+    // 渲染根组件 这个方法从导入的 Vue这个包中获得 渲染函数 h 可以代替 creElement
+    // render:function(creElement){
+    //     return creElement(App)
+    // }
+    // es6 的箭头函数
+    render: h => h(App)
 }).$mount('#app')
