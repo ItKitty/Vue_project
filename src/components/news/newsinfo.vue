e09c748f4945f766f0247207db4b5d3984dd60ae
wyf <wyf@gmail.com>
2017-12-31 16:00:20 +0800

    完成父元素通过props传值给子组件

--- src/components/news/newsinfo.vue
+++ src/components/news/newsinfo.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
+        <!-- 1.0 标题部分 -->
         <div class="titleStyle">
             <h5>{{newsInfo.title}}</h5>
             <p>
                 {{newsInfo.add_time | fmtDate}}&nbsp;&nbsp; {{newsInfo.click}}次浏览&nbsp;&nbsp; 分类:民生经济
             </p>
         </div>
-
+        <!-- 2.0 内容部分 -->
         <div class="contentStyle">
             <span v-html="newsInfo.content"></span>
         </div>
+
+        <!-- 3.0 评论部分 -->
+        <subcomment :comment-id="$route.params.newsId"></subcomment>
     </div>
 </template>
 
@@ -32,6 +36,9 @@ h5 {
 <script>
 // 导入公共的common.js
 import common from "../../common/common";
+// 导入子组件
+import subcomment from "../subcomponents/subcomment.vue"
+
 export default {
   data() {
     return {
@@ -52,6 +59,11 @@ export default {
         err => {}
       );
     }
+  },
+  components:{
+    //  名称是在template中使用的名称
+    //   subcomment:subcomment
+    subcomment
   }
 };
 </script>
---
+++ src/components/subcomponents/subcomment.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{commentId}}
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['commentId']
+    }
+</script>
